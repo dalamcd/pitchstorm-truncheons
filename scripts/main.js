8625ebf68cb3edb41d84443ee243a0b2f47d0cf1
negative1skunk@gmail.com <negative1skunk@gmail.com>
2020-10-26 16:12:37 -0500

    modified the teamselect.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,3 @@
-import { getTeams, useTeams } from "./teams/TeamProvider.js"
+import { TeamList } from "./teams/TeamList.js"
 
-console.log("Welcome to the main module")
-getTeams().then(()=>
-console.log(useTeams()))
-
-import { usePlayers, getPlayers } from "./players/PlayerProvider.js"
-
-console.log("Welcome to the main module")
-getPlayers().then(()=>
-console.log(usePlayers()))
+TeamList()
--- scripts/teams/TeamProvider.js
+++ scripts/teams/TeamProvider.js
@@ -1,13 +1,13 @@
-let teams=[]
+let teams = []
 
-export const useTeams=()=>{
-    return teams.slice()
+export const useTeams = () => {
+  return teams.slice()
 }
 
-export const getTeams=()=>{
-    return fetch("http://localhost:8088/teams")
-    .then(response=>response.json())
-    .then(parsedTeams=>{
-        teams=parsedTeams
+export const getTeams = () => {
+  return fetch("http://localhost:8088/teams")
+    .then((response) => response.json())
+    .then((parsedTeams) => {
+      teams = parsedTeams
     })
 }
