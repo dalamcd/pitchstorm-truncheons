2b9a653406d87b2ca06af278e6a61a9b155fd910
dalamcd <dalamcd@gmail.com>
2020-10-26 21:07:20 -0500

    Tested player dropdowns, fixed some bugs

--- scripts/Roster.js
+++ scripts/Roster.js
@@ -5,24 +5,24 @@ import { TeamSelect } from "./teams/TeamSelect.js"
 const eventHub = document.querySelector(".container");
 
 eventHub.addEventListener("rosterTeamSelected", e => {
-    playerMenus(e.detail.teamID);
+    playerMenus(e.detail.teamId);
 })
 
 export const playerMenus = (teamID) => {
 
-    const playerOne = document.querySelector(".manageRoster__Team__playerOne");
-    const playerTwo = document.querySelector(".manageRoster__Team__playerTwo");
-    const playerThree = document.querySelector(".manageRoster__Team__playerThree");
+    const playerOne = document.querySelector("#manageRoster__Team__playerOne");
+    const playerTwo = document.querySelector("#manageRoster__Team__playerTwo");
+    const playerThree = document.querySelector("#manageRoster__Team__playerThree");
 
     getPlayers().then(() => {
         const playerList = usePlayers();
-        const teamPlayers = playerList.map(player => { player.teamID == teamID });
+        const teamPlayers = playerList.filter(player => player.teamID === teamID)
 
-        playerOneHTML = `<option value=${teamPlayers[0].id}>${teamPlayers[0].firstName} 
+        let playerOneHTML = `<option value=${teamPlayers[0].id}>${teamPlayers[0].firstName} 
         ${teamPlayers[0].lastName}</option>`
-        playerTwoHTML = `<option value=${teamPlayers[1].id}>${teamPlayers[1].firstName} 
+        let playerTwoHTML = `<option value=${teamPlayers[1].id}>${teamPlayers[1].firstName} 
         ${teamPlayers[1].lastName}</option>`
-        playerThreeHTML = `<option value=${teamPlayers[2].id}>${teamPlayers[2].firstName} 
+        let playerThreeHTML = `<option value=${teamPlayers[2].id}>${teamPlayers[2].firstName} 
         ${teamPlayers[2].lastName}</option>`
 
         playerOneHTML += playerList.map(player => {
