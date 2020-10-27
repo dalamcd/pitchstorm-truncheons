fc53dc0ef6228340616840f7a7c930a2ff046cb7
dalamcd <dalamcd@gmail.com>
2020-10-26 21:13:07 -0500

    Added comments

--- scripts/Roster.js
+++ scripts/Roster.js
@@ -4,20 +4,30 @@ import { TeamSelect } from "./teams/TeamSelect.js"
 
 const eventHub = document.querySelector(".container");
 
+// Add event listener for when the team is changed
 eventHub.addEventListener("rosterTeamSelected", e => {
+    // Call playerMenu, passing in the ID of the selected team.
     playerMenus(e.detail.teamId);
 })
 
+// TODO: The roster loads with a team already selected, need to make sure to populate
+//       the drop down menus with whatever team is initially selected.
 export const playerMenus = (teamID) => {
 
+    // Get references to the DOM elements of the three players.
     const playerOne = document.querySelector("#manageRoster__Team__playerOne");
     const playerTwo = document.querySelector("#manageRoster__Team__playerTwo");
     const playerThree = document.querySelector("#manageRoster__Team__playerThree");
 
     getPlayers().then(() => {
+        // Get full list of players.
         const playerList = usePlayers();
+        // Look through the list of players and pick out only the players that are on the
+        // selected team.
         const teamPlayers = playerList.filter(player => player.teamID === teamID)
 
+        // Set up the drop down menus, with the first automatically selected option
+        // being the players on the team.
         let playerOneHTML = `<option value=${teamPlayers[0].id}>${teamPlayers[0].firstName} 
         ${teamPlayers[0].lastName}</option>`
         let playerTwoHTML = `<option value=${teamPlayers[1].id}>${teamPlayers[1].firstName} 
@@ -25,6 +35,8 @@ export const playerMenus = (teamID) => {
         let playerThreeHTML = `<option value=${teamPlayers[2].id}>${teamPlayers[2].firstName} 
         ${teamPlayers[2].lastName}</option>`
 
+        // Add the rest of the players to each drop down menu, making sure that we don't
+        // add the players that we started with in each one.
         playerOneHTML += playerList.map(player => {
             if (player.id != teamPlayers[0].id)
                 return `<option value=${player.id}>${player.firstName} ${player.lastName}</option>`
@@ -38,6 +50,7 @@ export const playerMenus = (teamID) => {
                 return `<option value=${player.id}>${player.firstName} ${player.lastName}</option>`
         })
 
+        // Add the HTML to the DOM.
         playerOne.innerHTML = playerOneHTML;
         playerTwo.innerHTML = playerTwoHTML;
         playerThree.innerHTML = playerThreeHTML;
