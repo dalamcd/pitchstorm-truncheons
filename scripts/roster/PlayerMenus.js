07cd4b5e3903999864efff78374295eda3d7caa9
dalamcd <dalamcd@gmail.com>
2020-11-05 13:32:11 -0600

    Fixed bugs in the select team player dropdowns

--- scripts/roster/PlayerMenus.js
+++ scripts/roster/PlayerMenus.js
@@ -11,32 +11,50 @@ export const playerMenus = (teamID) => {
 
     getPlayers().then(() => {
         // Get full list of players.
-        const playerList = usePlayers();
+        let playerList = usePlayers();
         // Look through the list of players and pick out only the players that are on the
         // selected team.
         const teamPlayers = playerList.filter(player => player.teamID === teamID)
-
+        console.log(teamPlayers);
         // Set up the drop down menus, with the first automatically selected option
         // being the players on the team.
-        let playerOneHTML = `<option value=${teamPlayers[0].id}>${teamPlayers[0].firstName} 
-        ${teamPlayers[0].lastName}</option>`
-        let playerTwoHTML = `<option value=${teamPlayers[1].id}>${teamPlayers[1].firstName} 
-        ${teamPlayers[1].lastName}</option>`
-        let playerThreeHTML = `<option value=${teamPlayers[2].id}>${teamPlayers[2].firstName} 
-        ${teamPlayers[2].lastName}</option>`
+        let playerOneHTML;
+        let playerTwoHTML;
+        let playerThreeHTML;
+
+        if (teamPlayers[0]) {
+            playerOneHTML = `<option value=${teamPlayers[0].id}>${teamPlayers[0].firstName} 
+            ${teamPlayers[0].lastName}</option>`
+             playerList = playerList.filter(player => player.id !== teamPlayers[0].id)
+        }
+        else
+            playerOneHTML = `<option value=0>No player selected</option>`
+
+        if (teamPlayers[1]) {
+            playerTwoHTML = `<option value=${teamPlayers[1].id}>${teamPlayers[1].firstName} 
+            ${teamPlayers[1].lastName}</option>`
+             playerList = playerList.filter(player => player.id !== teamPlayers[1].id)
+        }
+        else
+            playerTwoHTML = `<option value=0>No player selected</option>`
+
+        if (teamPlayers[2]) {
+            playerThreeHTML = `<option value=${teamPlayers[2].id}>${teamPlayers[2].firstName} 
+            ${teamPlayers[2].lastName}</option>`
+             playerList = playerList.filter(player => player.id !== teamPlayers[2].id)
+        }
+        else
+            playerThreeHTML = `<option value=0>No player selected</option>`
 
         // Add the rest of the players to each drop down menu, making sure that we don't
         // add the players that we started with in each one.
         playerOneHTML += playerList.map(player => {
-            if (player.id != teamPlayers[0].id)
                 return `<option value=${player.id}>${player.firstName} ${player.lastName}</option>`
         })
         playerTwoHTML += playerList.map(player => {
-            if (player.id != teamPlayers[1].id)
                 return `<option value=${player.id}>${player.firstName} ${player.lastName}</option>`
         })
         playerThreeHTML += playerList.map(player => {
-            if (player.id != teamPlayers[2].id)
                 return `<option value=${player.id}>${player.firstName} ${player.lastName}</option>`
         })
 
