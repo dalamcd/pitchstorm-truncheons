589a867b20a7937b13b0e555a2ee042524726950
dalamcd <dalamcd@gmail.com>
2020-11-06 14:03:21 -0600

    Fixed bugs with saving team changes

--- scripts/roster/modifyTeam.js
+++ scripts/roster/modifyTeam.js
@@ -7,45 +7,43 @@ export const changePlayerTeam = (playerID, teamID) => {
 
     return getPlayers().then(() => {
         const player = usePlayers().find(playerObj => playerObj.id === playerID);
-        if (player)
+        if (player) {
             player.teamID = teamID;
 
-        return fetch(`http://localhost:8088/players/${player.id}`, {
-            method: "PATCH",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(player)
-        });
+            return fetch(`http://localhost:8088/players/${player.id}`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(player)
+            });
+        }
     });
 }
 
-export const modifyTeam = () => {
+export const modifyTeam = (teamObj) => {
     getTeams().then(() => {
-        const teamID = useTeams().find(
-            team => team.id === parseInt(document.querySelector("#team-select-dropdown").value)).id;
 
-        const playerOne = parseInt(document.querySelector("#manageRoster__Team__playerOne").value);
-        const playerTwo = parseInt(document.querySelector("#manageRoster__Team__playerTwo").value);
-        const playerThree = parseInt(document.querySelector("#manageRoster__Team__playerThree").value);
-
-        changePlayerTeam(playerOne, teamID);
-        changePlayerTeam(playerTwo, teamID);
-        changePlayerTeam(playerThree, teamID);
+        let promiseChain = [];
+        //debugger
+        promiseChain.push(changePlayerTeam(teamObj.pOneId, teamObj.teamId));
+        promiseChain.push(changePlayerTeam(teamObj.pTwoId, teamObj.teamId));
+        promiseChain.push(changePlayerTeam(teamObj.pThreeId, teamObj.teamId));
 
         usePlayers().forEach(player => {
-            if (player.id !== playerOne && player.id !== playerTwo && player.id !== playerThree) {
-                if (player.teamID === teamID) {
-                    changePlayerTeam(player.id, 0).then(() => {
-                        eventHub.dispatchEvent(new CustomEvent("teamDropdownStateChanged", {
-                            detail: {
-                                teamID
-                            }
-                        }));
-                    });
+            if (player.id !== teamObj.pOneId && player.id !== teamObj.pTwoId && player.id !== teamObj.pThreeId) {
+                if (player.teamID === teamObj.teamId) {
+                    promiseChain.push(changePlayerTeam(player.id, 0));
                 }
             }
         });
-
+        Promise.all(promiseChain)
+            .then(() => {
+                eventHub.dispatchEvent(new CustomEvent("teamDropdownStateChanged", {
+                    detail: {
+                        teamID: teamObj.teamId
+                    }
+                }));
+            })
     });
 }
